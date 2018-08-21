d6c785e1e418a80e548477c87ffa983ceb2cfbdb
Denilson Gama <denilson.gama@gamadeveloper.com.br>
2018-08-21 20:41:01 -0300

    first commit: Tela Hello World

---
+++ app/src/main/java/br/com/gamadeveloper/bronapp/page_login.kt
@@ -0,0 +1,12 @@
+package br.com.gamadeveloper.bronapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class page_login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page_login)
+    }
+}
